430bcc32650da826aff2735100b8e87c7ca36a24
maharshiPatel9094 <144848033+maharshiPatel9094@users.noreply.github.com>
2024-05-05 16:47:55 -0400

    user route added

--- fullstack_nextjs_app/src/app/api/sign-up/route.ts
+++ fullstack_nextjs_app/src/app/api/sign-up/route.ts
@@ -7,17 +7,112 @@ export async function POST(request: Request) {
   await dbConnect();
 
   try {
-    const {username, email, password}  = await request.json()
+    const { username, email, password } = await request.json();
+
+    const existingVerifiedUserByUsername = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (existingVerifiedUserByUsername) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username is already taken",
+        },
+        { status: 400 }
+      );
+    }
+
+    const existingUserByEmail = await UserModel.findOne({ email });
+    let verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+    // if user exist
+    if (existingUserByEmail) {
+      // if user is verified
+      if (existingUserByEmail.isVerified) {
+        return Response.json(
+          {
+            success: false,
+            message: "User already exists with this email",
+          },
+          { status: 400 }
+        );
+      }
+      //update the user if not verified
+      else {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        existingUserByEmail.password = hashedPassword;
+        existingUserByEmail.verifyCode = verifyCode;
+        existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000);
+        await existingUserByEmail.save();
+      }
+    }
+    // if user does not exist
+    else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const expiryDate = new Date();
+      expiryDate.setHours(expiryDate.getHours() + 1);
+      // new user
+      const newUser = new UserModel({
+        username,
+        email,
+        password: hashedPassword,
+        verifyCode,
+        verifyCodeExpiry: expiryDate,
+        isVerified: false,
+        isAcceptingMessages: true,
+        messages: [],
+      });
+      // save the user
+      await newUser.save();
+    }
+
+    // Send verification email
+    const emailResponse = await sendVerificationEmail(
+      email,
+      username,
+      verifyCode
+    );
+    if (!emailResponse.success) {
+      return Response.json(
+        {
+          success: false,
+          message: emailResponse.message,
+        },
+        { status: 500 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "User registered successfully. Please verify your account.",
+      },
+      { status: 201 }
+    );
   } catch (error) {
-    console.error(" Error Registering The User ", error);
+    console.error("Error registering user:", error);
     return Response.json(
       {
         success: false,
-        message: " Error Registering User ",
+        message: "Error registering user",
       },
-      {
-        status: 500,
-      }
+      { status: 500 }
     );
   }
 }
+
+// algorithm used
+
+/* 
+if existingUserByEmail EXISTS THEN
+    if existingUserByEmail.isVerified THEN 
+        success: false
+    else 
+        // save the updated user
+    ENDIF
+else
+    // create a new user with provided details
+    // save the new user
+endif
+*/
